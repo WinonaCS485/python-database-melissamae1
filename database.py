169574480ea5cfef10633e1a041142c807aad6b5
Melissa Danielson <59889711+melissamae1@users.noreply.github.com>
2020-02-01 14:53:04 -0600

    complete with updates

--- database.py
+++ database.py
@@ -8,35 +8,24 @@ connection = pymysql.connect(host='mrbartucz.com',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+
 try:
     with connection.cursor() as cursor:
-        # Select all Students
-        #sql = "SELECT * from Students"
-        
-        # execute the SQL command
-        #cursor.execute(sql)
-        
-        # get the results
-        #for result in cursor:
-            #print (result)
-        
-      
-        # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
-        # So you must commit to save your changes. 
-        # connection.commit()
 
         #search by user input
         name = input("Enter a first name to search: ")
+        sql = "SELECT * from Students WHERE firstName LIKE %s"
 
         # execute the SQL command
-        cursor.execute("SELECT * from Students WHERE firstName LIKE %s", (name,))
-            
+        cursor.execute(sql, (name,))
+                
         # get the results
         for result in cursor:
             print (result)
-
+                
+            
 finally:
-    connection.close()
+    connection.close()  
 
 
 
